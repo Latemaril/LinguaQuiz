2de9512ddd2f415211905ec348e46d1ad0ec50cf
Latemaril <sukhovey.vlad@gmail.com>
2023-05-23 12:37:56 +1000

    work

--- wp-content/themes/photobook_wp_react/page-templates/trial-quiz_page.php
+++ wp-content/themes/photobook_wp_react/page-templates/trial-quiz_page.php
@@ -7,13 +7,53 @@ get_header();
 
 ?>
 <main id="l-quiz-page">
+    <div class="bg-animation">
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+    </div>
     <div id="l-quiz">
-        <div class="m-quiz-form"></div>
-        <div class="m-quiz-form"></div>
-        <div class="m-quiz-form"></div>
-        <div class="m-quiz-form"></div>
-        <div class="m-quiz-form"></div>
-        <div class="m-quiz-form"></div>
+        <h1 class="quiz-main-title">Time to Quiz</h1>
+        <div class="m-quiz-form" data-number="1">
+            <div class="m-question">
+                <p>Слово (당근) означает определенный корнеплод, но так как первые согласные в слогах слова совпадают с согласными в другом слове (당연), которое переводится на русский как “конечно”, то корейцы часто используют первое вместо второго просто забавы ради. В русском же языке этот овощ идёт рука об руку с любовью.
+                    Какой корнеплод используют корейцы в форме согласия?</p>
+            </div>
+            <div class="m-answers">
+                <div>
+                    <input type="radio" id="control_01" name="select" value="1" checked>
+                    <label for="control_01">
+                        <h5>Pfft</h5>
+                    </label>
+                </div>
+                <div>
+                    <input type="radio" id="control_01" name="select" value="1" checked>
+                    <label for="control_01">
+                        <h5>Pfft</h5>
+                    </label>
+                </div>
+                <div>
+                    <input type="radio" id="control_01" name="select" value="1" checked>
+                    <label for="control_01">
+                        <h5>Pfft</h5>
+                    </label>
+                </div>
+                <div>
+                    <input type="radio" id="control_01" name="select" value="1" checked>
+                    <label for="control_01">
+                        <h5>Pfft</h5>
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="m-quiz-form" data-number="2"></div>
+        <div class="m-quiz-form" data-number="3"></div>
+        <div class="m-quiz-form" data-number="4"></div>
+        <div class="m-quiz-form" data-number="5"></div>
+        <div class="m-quiz-form" data-number="6"></div>
     </div>
 </main>
 
